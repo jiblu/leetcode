eb8437f88697b575b2dfe12ecd4c6536236d55f0
ji <jkim0107@gmail.com>
2020-01-27 16:11:14 -0800

    is palindrome solution

---
+++ easy/125_isPalindrome.js
@@ -0,0 +1,35 @@
+/*
+125. Valid Palindrome
+Easy
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+*/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  // create string without spaces or other punctuation
+  let alphanum ='abcdefghijklmnopqrstuvwxyz0123456789'
+  let array = s.toLowerCase().split('')
+  let filteredArray = array.filter((char) => {
+    return alphanum.indexOf(char) >= 0
+  })
+  return filteredArray.join('') === filteredArray.reverse().join('')
+}
+
+/*
+Runtime: 76 ms, faster than 56.58% of JavaScript online submissions for Valid Palindrome.
+Memory Usage: 40 MB, less than 21.74% of JavaScript online submissions for Valid Palindrome.
+*/
